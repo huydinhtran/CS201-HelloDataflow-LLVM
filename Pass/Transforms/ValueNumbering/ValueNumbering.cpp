c9af92c0ae393ed28612cb01b015512137b8a660
huydinhtran <91640334+huydinhtran@users.noreply.github.com>
2022-03-08 03:02:05 -0800

    Update ValueNumbering.cpp

--- Pass/Transforms/ValueNumbering/ValueNumbering.cpp
+++ Pass/Transforms/ValueNumbering/ValueNumbering.cpp
@@ -5,6 +5,7 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/FormatVariadic.h"
 #include "llvm/IR/CFG.h"
+#include <set>
 
 using namespace std;
 
@@ -17,17 +18,14 @@ struct table{
 };
 
 struct blockContent{
-    StringRef liveIn[10];
-    StringRef liveOut[10];
-    StringRef ueVar[10];
-    StringRef varKill[10];
+    std::vector<StringRef> liveOut;
+    std::vector<StringRef> ueVar;
+    std::vector<StringRef> varKill;
     llvm::BasicBlock* block;
     StringRef name;
     int blockId;
-    llvm::BasicBlock* pred;
-    llvm::BasicBlock* succ;
+    llvm::BasicBlock* succ[10];
 };
-
 
 
 struct table hTable[10];
@@ -38,8 +36,8 @@ int tableCount = loadCount;
 int ueVarCount = 0;
 int varKillCount = 0;
 bool loadFound = false;
-bool first = true;
 bool afterOp = false;
+
 
 namespace
 {
@@ -63,11 +61,6 @@ namespace
             blockCon[blockCount].block = &basic_block;
             blockCon[blockCount].name = basic_block.getName();
             blockCon[blockCount].blockId = blockCount;
-            blockCon[blockCount].pred = basic_block.getSinglePredecessor();
-            blockCon[blockCount].succ = basic_block.getSingleSuccessor();
-            // errs() << blockCon[blockCount].pred << "\n";
-            // errs() << blockCon[blockCount].succ << "\n";
-            errs() << "2 PREDS? "<<basic_block.hasNPredecessors (2) << "\n";
             
             errs() << blockCon[blockCount].blockId << "\n";
             errs() << blockCon[blockCount].block << "\n";
@@ -80,6 +73,7 @@ namespace
                 llvm::Value* var2 = inst.getOperand(1);
                 StringRef var2Name = inst.getOperand(1)->getName();
                 loadFound = false;
+
 
                 if (inst.getOpcode() == Instruction::Load)
                 {
@@ -96,11 +90,9 @@ namespace
                 {
                     if (inst.getOpcode() == Instruction::Add)
                     {
-                        blockCon[blockCount].ueVar[ueVarCount] = hTable[loadCount-2].name;
-                        blockCon[blockCount].ueVar[ueVarCount+1] = hTable[loadCount-1].name;
-                        StringRef name1 = hTable[loadCount-2].name;
-                        StringRef name2 = hTable[loadCount-1].name;
-                        errs() << formatv("{0,-40}", inst) << "UEVAR: " << name1 << " " << name2 << "\n";
+                        blockCon[blockCount].ueVar.insert(blockCon[blockCount].ueVar.end(),hTable[loadCount-2].name);
+                        blockCon[blockCount].ueVar.insert(blockCon[blockCount].ueVar.end(),hTable[loadCount-1].name);
+                        errs() << formatv("{0,-40}", inst) << "UEVAR: " << hTable[loadCount-2].name << " " << hTable[loadCount-1].name << "\n";
                         ueVarCount += 2;
                         afterOp = true;
                     }
@@ -110,16 +102,16 @@ namespace
                     if(afterOp == true){
                         hTable[tableCount].var = var2;
                         hTable[tableCount].name = var2Name;
-                        blockCon[blockCount].varKill[ueVarCount] = hTable[tableCount].name;
-                        errs() << formatv("{0,-40}", inst) << "VARKILL: " << blockCon[blockCount].varKill[ueVarCount] << "\n";
+                        blockCon[blockCount].varKill.insert(blockCon[blockCount].varKill.end(),hTable[loadCount].name);
+                        errs() << formatv("{0,-40}", inst) << "VARKILL: " << hTable[tableCount].name << "\n";
                         varKillCount++;
                         tableCount++;
                         afterOp = false;
                     }else{
-                        blockCon[blockCount].ueVar[ueVarCount] = hTable[loadCount-1].name;
-                        blockCon[blockCount].varKill[ueVarCount] = hTable[loadCount-2].name;
-                        errs() << formatv("{0,-40}", inst) << "UEVAR: " << blockCon[blockCount].ueVar[ueVarCount] << "\n";
-                        errs() << formatv("{0,-40}", inst) << "VARKILL: " << blockCon[blockCount].varKill[ueVarCount] << "\n";
+                        blockCon[blockCount].ueVar.insert(blockCon[blockCount].ueVar.end(),hTable[loadCount-1].name);
+                        blockCon[blockCount].varKill.insert(blockCon[blockCount].varKill.end(),hTable[loadCount-2].name);
+                        errs() << formatv("{0,-40}", inst) << "UEVAR: " << hTable[loadCount-1].name << "\n";
+                        errs() << formatv("{0,-40}", inst) << "VARKILL: " << hTable[loadCount-2].name << "\n";
                         ueVarCount++;
                         varKillCount++;
                         tableCount++;
@@ -141,24 +133,60 @@ namespace
 
         // }
 
+        errs() << "//////////////////////////////Implementing LiveOut//////////////////////////////" << "\n";
+        // std::vector<StringRef> v1; 
+        // std::vector<StringRef> v2 = {"3f", "4g", "5h", "6i", "7j"}; 
+        // std::vector<StringRef> dest1; // create a third vector to write results
+        // v1.insert(v1.end(),"concac");
+        // v1.insert(v1.end(),"vkl");
+        // std::set_union(v1.begin(), v1.end(), v2.begin(), v2.end(),std::back_inserter(dest1));
+        // for(int i=0 ; i < 8; i++){
+        //     errs() << "dest1    " << dest1[i] <<"\n";
+        // }
 
 
-        for (auto &basic_block : F)
-        {
-            for (BasicBlock *Pred : predecessors(&basic_block)) {
-
-                errs() << basic_block << "\n";
-                errs() << Pred->getName() << "\n";
-                // for(int i=0 ; i < (sizeof(blockCon[blockCount].ueVar)/sizeof(blockCon[blockCount].ueVar[0])) ; i++){
-                //     if(blockCon[blockCount].ueVar[i] != ""){
-                //         errs() << "ueVar: " << blockCon[blockCount].ueVar[i] << "\n";
-                //     }
-                // }
-                // // errs() << "ueVar: " << blockCon[blockCount].ueVar << "\n";
-                // // errs() << "varKill: " << blockCon[blockCount].varKill << "\n";
-                // blockCount--;
+        // int succLoop=0;
+        // errs() << blockCon[2].name << "\n";
+        for (int i = blockCount; i > 0; i--){
+            for (BasicBlock *Succ : successors(blockCon[i].block)) {
+                for(int j = 0; j < blockCount; j++){
+                    errs() << "Succ" << Succ<<"\n";
+                    errs() << "block" << blockCon[j].block<<"\n";
+                    errs() << "\n";
+                    if (Succ == blockCon[j].block){
+                        std::vector<StringRef> kill_U_UE;
+                        std::set_union(blockCon[j].varKill.begin(), blockCon[j].varKill.end(), blockCon[j].ueVar.begin(), blockCon[j].ueVar.end(),std::back_inserter(kill_U_UE));
+                        errs() << "print kill_U_UE" << "\n";
+                        for (auto k: kill_U_UE){
+                            errs() << k << ' ';
+                        }
+                        std::vector<StringRef> out_diff;
+                        std::set_difference(blockCon[j].liveOut.begin(), blockCon[j].liveOut.end(), kill_U_UE.begin(), kill_U_UE.end(),std::back_inserter(out_diff));
+                        std::set_union(blockCon[i].liveOut.begin(), blockCon[i].liveOut.end(), out_diff.begin(), out_diff.end(),std::back_inserter(blockCon[i].liveOut));
+                    }
+                }
+                // errs() << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" << "\n";
+                // errs() << "liveOut:   " << blockCon[i].liveOut[0] << "\n";
             }
         }
+
+
+        // for (auto &basic_block : F)
+        // {
+        //     for (BasicBlock *Pred : predecessors(&basic_block)) {
+
+        //         errs() << basic_block << "\n";
+        //         errs() << Pred->getName() << "\n";
+        //         // for(int i=0 ; i < (sizeof(blockCon[blockCount].ueVar)/sizeof(blockCon[blockCount].ueVar[0])) ; i++){
+        //         //     if(blockCon[blockCount].ueVar[i] != ""){
+        //         //         errs() << "ueVar: " << blockCon[blockCount].ueVar[i] << "\n";
+        //         //     }
+        //         // }
+        //         // // errs() << "ueVar: " << blockCon[blockCount].ueVar << "\n";
+        //         // // errs() << "varKill: " << blockCon[blockCount].varKill << "\n";
+        //         // blockCount--;
+        //     }
+        // }
 
 
 
